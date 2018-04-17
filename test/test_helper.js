738db506d1004b0282771a9a693c2787c80f1cf1
RSPACE150\alon <alon.b@appnext.com>
2018-04-17 10:39:04 +0300

    first commit

---
+++ test/test_helper.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+before((done) => {
+
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection.once('open', () => {
+
+    done();
+
+  }).on('error', (error) => {
+    console.log('Warning', error);
+  });
+
+});
+
+beforeEach((done) => {
+ mongoose.connection.collections.users.drop(() => {
+   // ready to run the next step
+   done();
+ });
+});
